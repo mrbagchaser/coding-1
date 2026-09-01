7b11a4dba49131cb8812525196741307595e8e3c
mrbagchaser <daysonmosley53@gmail.com>
2026-09-01 12:45:38 +0000

    first commit

---
+++ unit 1/datatypes.py
@@ -0,0 +1,8 @@
+#The hashtag is a maker that makes anything written 
+# without a decimal point
+# we dont have to add any special symbolys. we can write the number 
+# as is
+# floats- a number with a decimal
+#Boolean- a data type that can be either ture or false
+# we do not use quotation marks for boolean values. the computer
+# understands these words as something unique.
