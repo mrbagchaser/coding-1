e7810912300ab56d230c225c36cf350022332bee
mrbagchaser <daysonmosley53@gmail.com>
2026-09-03 12:52:13 +0000

    Variable

---
+++ variableActivity.py
@@ -0,0 +1,36 @@
+# variable- a container that stores data. you can
+# name your container almost anything you want. so long
+# as it is descriptive
+
+name = "Dayson Mosley"
+
+# when we want to create a variable we make up a word
+#use the equal sign and then assign it a data type
+# whatever the computer sees our variable word it will
+# always use the data inside  
+
+# Variable naming conventions
+#Camel case-
+# Snake Case-
+# Pascal case-
+
+
+
+
+
+# Camel case- when creating variable names with multiplke words
+#Every new word begins with a capital letter
+# The reason why its called Camel case is bc every new
+# Represents a hump.
+
+
+# Snake case- when creating a variable using snake
+# we use a underscore symbol to space out each new word
+# The reason why its called snake case is bc the underscore
+# Represents a snake on the ground
+
+
+
+#pascal case- pascal case is the same as camel case Except
+# all words starts with a capital letter
+# its called pascal case bc its the same way we write first/ last names
