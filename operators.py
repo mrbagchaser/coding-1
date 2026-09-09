bfdc2ceb136cbe6922fa43126bfbcc43d7231d86
mrbagchaser <daysonmosley53@gmail.com>
2026-09-09 12:53:41 +0000

    notes

---
+++ operators.py
@@ -0,0 +1,26 @@
+# Assignment operators are exclusively used to assign values to variables
+#key/value pairs
+#
+
+
+# we use a single equal sign to represent the assignment operator
+#name = ''Dayson Mosley''
+#grade=10
+#school= true
+#Arithmetic operators are used to perform numerical
+# data types to perfrom calculations 
+#intergers (whole numbers) and floats( decimal numbers)
+#print is a funtion that lets us show code
+# in the terminal
+print(3+3) #addition operator
+print(3-3) #subtraction operator
+print(12/3) #division operator 
+print(3*4) #multiplication operator  
+
+
+#comparsion operators- Set of symbols used
+# to assess if data is the same or different and
+# how they differ
+
+print(10>1) #greater than operator
+print(10<1) #less than operator
